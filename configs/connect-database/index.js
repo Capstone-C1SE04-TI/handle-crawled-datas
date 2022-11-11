91e8b0b0d1f89b2683c460c1368af3537fe40e0a
huynhdthanhtuan <tuan40655@gmail.com>
2022-11-11 20:07:05 +0700

    Refactor: Remove DB Crawl Import Statement

--- configs/connect-database/index.js
+++ configs/connect-database/index.js
@@ -2,11 +2,9 @@ const { initializeApp, cert } = require("firebase-admin/app");
 const { getFirestore } = require("firebase-admin/firestore");
 
 const serviceAccountMain = require("./service-account-Capstone-C1SE04-TI-Main.json");
-const serviceAccountCrawl = require("./service-account-Capstone-C1SE04-TI-Crawl.json");
 
 initializeApp({
     credential: cert(serviceAccountMain),
-    // credential: cert(serviceAccountCrawl),
 });
 
 const database = getFirestore();
